eabf273df1954cd733a188b091ff67db32e3da15
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-11-27 00:33:00 +0300

    initial commit

---
+++ src/main/java/com/web/time_to_book/TimeToBookApplication.java
@@ -0,0 +1,13 @@
+package com.web.time_to_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeToBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeToBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/time_to_book/TimeToBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.time_to_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeToBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
