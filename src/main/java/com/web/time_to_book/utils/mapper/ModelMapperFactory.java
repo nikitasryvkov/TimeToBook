c7477722f5ad6efe22ea4559142a58a426b660ec
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-12-01 15:33:08 +0300

    fix: add mapping

--- src/main/java/com/web/time_to_book/utils/mapper/ModelMapperFactory.java
+++ src/main/java/com/web/time_to_book/utils/mapper/ModelMapperFactory.java
@@ -2,6 +2,11 @@ package com.web.time_to_book.utils.mapper;
 
 import org.modelmapper.ModelMapper;
 
+import com.web.time_to_book.dtos.request.UserRequestDTO;
+import com.web.time_to_book.dtos.response.UserResponseDTO;
+import com.web.time_to_book.models.Feedback;
+import com.web.time_to_book.models.User;
+
 public class ModelMapperFactory {
 
     private ModelMapper modelMapper;
@@ -15,6 +20,34 @@ public class ModelMapperFactory {
     }
 
     private ModelMapper initializeModelMapper(ModelMapper modelMapper) {
+
+        modelMapper
+                .typeMap(User.class, UserRequestDTO.class)
+                .addMappings(mapper -> {
+                    mapper.map(user -> user.getFirstName(), UserRequestDTO::setFirstName);
+                    mapper.map(user -> user.getLastName(), UserRequestDTO::setLastName);
+                    mapper.map(user -> user.getUsername(), UserRequestDTO::setUsername);
+                    mapper.map(user -> user.getEmail(), UserRequestDTO::setEmail);
+                    mapper.map(user -> user.getPassword(), UserRequestDTO::setPassword);
+                    mapper.map(user -> user.getPhoneNumber(), UserRequestDTO::setPhoneNumber);
+                    mapper.map(user -> user.getAvatarURL(), UserRequestDTO::setAvatarURL);
+                    mapper.map(user -> user.getPassword(), UserRequestDTO::setPassword);
+                    mapper.map(user -> user.getRole().getName(), UserRequestDTO::setRoleName);
+                });
+
+        modelMapper
+                .typeMap(User.class, UserResponseDTO.class)
+                .addMappings(mapper -> {
+                    mapper.map(user -> user.getFirstName(), UserResponseDTO::setFirstName);
+                    mapper.map(user -> user.getLastName(), UserResponseDTO::setLastName);
+                    mapper.map(user -> user.getUsername(), UserResponseDTO::setUsername);
+                    mapper.map(user -> user.getEmail(), UserResponseDTO::setEmail);
+                    mapper.map(user -> user.getPassword(), UserResponseDTO::setPassword);
+                    mapper.map(user -> user.getPhoneNumber(), UserResponseDTO::setPhoneNumber);
+                    mapper.map(user -> user.getAvatarURL(), UserResponseDTO::setAvatarURL);
+                    mapper.map(user -> user.getPassword(), UserResponseDTO::setPassword);
+                    mapper.map(user -> user.getNumberOfAppointments(), UserResponseDTO::setNumberOfAppointments);
+                });
 
         return modelMapper;
     }
