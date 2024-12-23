fb32b729b845a5ede0cdc4ea09a14983518f7d25
Nikita Sryvkov <n.sryvkov@yandex.ru>
2024-12-23 05:03:59 +0300

    fix: change exception

--- src/main/java/com/web/time_to_book/exceptions/role/RoleNotFoundException.java
+++ src/main/java/com/web/time_to_book/exceptions/role/RoleNotFoundException.java
@@ -1,7 +1,13 @@
 package com.web.time_to_book.exceptions.role;
 
+import com.web.time_to_book.models.enums.UserRoles;
+
 public class RoleNotFoundException extends RuntimeException {
     public RoleNotFoundException(String name) {
         super("Роли '" + name + "' не существует!");
+    }
+
+    public RoleNotFoundException(UserRoles role) {
+        super("Роли '" + role + "' не существует!");
     }
 }
